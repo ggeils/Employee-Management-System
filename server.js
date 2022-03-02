e8bec99abf8a818a150673aeda3c7fee37d99662
George Geils <95538913+ggeils@users.noreply.github.com>
2022-03-01 23:47:31 -0500

    added view functions and addDepartment function to server.js

--- server.js
+++ server.js
@@ -1,70 +1,138 @@
-const path = require('path');
-const fs = require('fs');
-let mysql = require('mysql');
+const path = require("path");
+const fs = require("fs");
+let mysql = require("mysql");
 let inquirer = require("inquirer");
-const connection = require('./connection.js');
+const connection = require("./connection.js");
 
 function init() {
-    console.log('You have successfully logged into the Employee Management System!')
-    inquirer.prompt([{
-      type: "list",
-      name: "start",
-      message: "What would you like to do?",
-      choices: [
-        {
+  console.log(
+    "You have successfully logged into the Employee Management System!"
+  );
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "start",
+        message: "What would you like to do?",
+        choices: [
+          {
             name: "View all departments",
-            value: "VIEW_DEPARTMENTS"
-    
-        },
-        {
+            value: "VIEW_DEPARTMENTS",
+          },
+          {
             name: "View all roles",
-            value: "VIEW_ROLES"
-        },
-        {
-          name: "View all employees",
-          value: "VIEW_EMPLOYEES"
-        },
-        {
+            value: "VIEW_ROLES",
+          },
+          {
+            name: "View all employees",
+            value: "VIEW_EMPLOYEES",
+          },
+          {
             name: "Add a department",
-            value: "ADD_DEPARTMENT"
-        },
-        {
+            value: "ADD_DEPARTMENT",
+          },
+          {
             name: "Add a role",
-            value: "ADD_ROLE"
-        },
-        {
-          name: "Add an employee",
-          value: "ADD_EMPLOYEE"
-        },
-        {
-          name: "Update an employee role",
-          value: "UPDATE_EMPLOYEE"
-        },
-  
-      ]
-    }]).then(({start}) => {
+            value: "ADD_ROLE",
+          },
+          {
+            name: "Add an employee",
+            value: "ADD_EMPLOYEE",
+          },
+          {
+            name: "Update an employee role",
+            value: "UPDATE_EMPLOYEE",
+          },
+        ],
+      },
+    ])
+    .then(({ start }) => {
       if (start == "VIEW_DEPARTMENTS") {
-          viewDepartments();
+        viewDepartments();
       }
       if (start == "VIEW_ROLES") {
-          viewRoles();
+        viewRoles();
       }
       if (start == "VIEW_EMPLOYEES") {
-        viewEmployeees();
-    }
-    if (start == "ADD_DEPARTMENT") {
+        viewEmployees();
+      }
+      if (start == "ADD_DEPARTMENT") {
         addDepartment();
-    }
-    if (start == "ADD_ROLE") {
+      }
+      if (start == "ADD_ROLE") {
         addRole();
-    }
-    if (start == "ADD_EMPLOYEE") {
+      }
+      if (start == "ADD_EMPLOYEE") {
         addEmployee();
-    }
-    if (start == "UPDATE_EMPLOYEE") {
+      }
+      if (start == "UPDATE_EMPLOYEE") {
         updateEmployee();
-    }
-    })
-  }
+      }
+    });
+}
 
-  init();
+function viewDepartments() {
+  console.log("Pulling all departments... \n");
+  connection.query(
+    "SELECT id AS `ID`, department AS `Department` FROM departments",
+    function (err, res) {
+      if (err) throw err;
+      console.table(res);
+      repeat();
+    }
+  );
+}
+
+function viewRoles() {
+  console.log("Pulling all roles... \n");
+  connection.query(
+    "SELECT title AS `Title`, salary AS `Salary`, department_id AS `Department ID` FROM roles",
+    function (err, res) {
+      if (err) throw err;
+      console.table(res);
+      repeat();
+    }
+  );
+}
+
+function viewEmployees() {
+  console.log("Pulling all employees... \n");
+  connection.query(
+    "SELECT first_name AS `First Name`, last_name AS `Last Name`, role_id AS `Role ID` FROM employees",
+    function (err, res) {
+      if (err) throw err;
+      console.table(res);
+      repeat();
+    }
+  );
+}
+
+function addDepartment() {
+  connection.query("SELECT * FROM departments", function (err, res) {
+    if (err) throw err;
+    let departments = res.map((element) => {
+      return element.id;
+    });
+    inquirer
+      .prompt([
+        {
+          name: "department",
+          type: "input",
+          message: "What is the department called?",
+        },
+      ])
+      .then(function (answer) {
+        connection.query(
+          "INSERT INTO departments SET ?",
+          answer,
+          function (err) {
+            if (err) throw err;
+            console.log(`${answer.department} was added to the database!`);
+            repeat();
+          }
+        );
+      });
+  });
+}
+
+init();
